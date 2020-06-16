1c890abefb2040067fff24d02355cf2ec3ee04b9
Nicola <lanzoni.nicola@gmail.com>
2020-06-16 18:30:07 +0200

    Updates

---
+++ precache-manifest.5840fd8b592e086ea829aa05b98bd5aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f87a1e58dcbde81425196138d45acc",
+    "url": "/ztm-robofriends-2020/index.html"
+  },
+  {
+    "revision": "0193a031907c0c9e6211",
+    "url": "/ztm-robofriends-2020/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0193a031907c0c9e6211",
+    "url": "/ztm-robofriends-2020/static/js/2.d8e543f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ztm-robofriends-2020/static/js/2.d8e543f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1aae0084f4be43ae66",
+    "url": "/ztm-robofriends-2020/static/js/main.0ad43529.chunk.js"
+  },
+  {
+    "revision": "ae543686d69ae5615abe",
+    "url": "/ztm-robofriends-2020/static/js/runtime-main.32b7716e.js"
+  }
+]);
